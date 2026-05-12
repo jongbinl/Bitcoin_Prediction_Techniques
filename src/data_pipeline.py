e5207deb47d8baf33f808c566e1411b3bdb1b9e5
Jongbin Lee <112026309+jongbinl@users.noreply.github.com>
2026-05-11 19:36:43 -0500

    Remove docstring from fetch_binance_data function
    Removed docstring for the fetch_binance_data function.

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -2,9 +2,7 @@ import yfinance as yf
 import pandas as pd
 
 def fetch_binance_data(symbol='BTC-USD', interval='1d', start_date='2023-01-01', end_date='2023-03-01'):
-    """
-    Fetch historical Bitcoin data using yfinance.
-    """
+    
     df = yf.download(symbol, start=start_date, end=end_date, interval='1d')
     df = df[['Close', 'Volume']].droplevel(1, axis=1).rename(columns={'Close': 'close', 'Volume': 'volume'})
     df.reset_index(inplace=True)
